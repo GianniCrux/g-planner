4b33700cd5fbd50f9bbfbff1cae5e6f48082ee6b
GianniCrux <gianni.disti@gmail.com>
2024-05-11 16:29:04 +0200

    removed wrong spacing in the organization list

--- app/(dashboard)/_components/sidebar/list.tsx
+++ app/(dashboard)/_components/sidebar/list.tsx
@@ -13,7 +13,7 @@ export const List = () => {
     if (!userMemberships.data?.length) return null;
 
     return (
-        <ul className="space-y-4 grid grid-cols-3 gap-4 ">
+        <ul className="grid grid-cols-3 gap-4 mt-4">
             {userMemberships.data?.map((mem) => (
             <Item 
                 key={mem.organization.id}
