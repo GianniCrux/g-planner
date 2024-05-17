cc269a4367a6ed0f6fca17e3153e806b72c60cb5
GianniCrux <gianni.disti@gmail.com>
2024-05-17 16:42:48 +0200

    organization setup

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,14 +1,23 @@
-import { OrganizationSwitcher, UserButton } from "@clerk/nextjs";
+"use client";
+
+import { 
+    UserButton, 
+    useOrganization,
+ } from "@clerk/nextjs";
 import { SearchInput } from "./search-input";
 import { InviteButton } from "./invite-button";
 
 export const Navbar = () => {
+    const { organization } = useOrganization();
+
     return (
         <div className="flex items-center gap-x-4 p-5 relative">
             <div className="lg:flex-1">
                 <SearchInput />
             </div>
+            {organization && (
             <InviteButton />
+            )}
             <UserButton />
         </div>
     );
