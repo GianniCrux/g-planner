a64df7b186fc13e422ef94da2c68d7c2c98493d7
GianniCrux <gianni.disti@gmail.com>
2024-06-03 17:40:45 +0200

    corrected toggleDialog for closing the form dialog

--- app/(dashboard)/_components/task-list.tsx
+++ app/(dashboard)/_components/task-list.tsx
@@ -105,7 +105,7 @@ export const TaskList = ({
                     <Plus className="mr-2 h-4 w-4" /> Add Task
                   </Button>
                   {showDialog && (
-                    <Dialog open={showDialog}>
+                    <Dialog open={showDialog} onOpenChange={toggleDialog}>
                       <DialogContent className="bg-amber-200">
                         <CardCreator onClose={toggleDialog} />
                       </DialogContent>
