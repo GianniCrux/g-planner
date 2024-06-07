ed4de526b66556e7edccb45d886c9d1c51a6bf7b
GianniCrux <gianni.disti@gmail.com>
2024-06-07 21:11:14 +0200

    implemented button to switch from month, week, and day view

--- app/(dashboard)/_components/calendar-task.tsx
+++ app/(dashboard)/_components/calendar-task.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react';
 import { Button } from "@/components/ui/button";
-import { Calendar } from "lucide-react";
+import { Calendar, CalendarDaysIcon, EyeIcon } from "lucide-react";
 import { Calendar as BigCalendar, momentLocalizer} from 'react-big-calendar';
 import moment from 'moment';
 import 'react-big-calendar/lib/css/react-big-calendar.css';
@@ -24,7 +24,7 @@ export const CalendarTask = ({ tasks }: CalendarTaskProps) => {
   const [showDialog, setShowDialog] = useState(false);
   const [selectedDate, setSelectedDate] = useState<Date | null>(null);
   const [tasksForSelectedDate, setTasksForSelectedDate] = useState<CalendarEvent[]>([]);
-
+  const [currentView, setCurrentView] = useState<'month' | 'week' | 'day' | 'work_week' | 'agenda'>('month');
 
   useEffect(() => {
     const calendarEvents: CalendarEvent[] = tasks.filter((task) => task.date).map((task) => ({
@@ -51,30 +51,49 @@ export const CalendarTask = ({ tasks }: CalendarTaskProps) => {
     setTasksForSelectedDate([]);
   };
 
+  const handleViewChange = () => {
+    setCurrentView((prevView) => {
+      if (prevView === 'month') return 'week';
+      if (prevView === 'week') return 'day';
+      if (prevView === 'day') return 'month';
+      return 'month';
+    });
+  };
+
+
   return (
     <div className="relative bg-transparent text-xl shadow-lg overflow-hidden p-8 md:p-16">
   <div className="absolute inset-0 opacity-70">
     <div className="absolute inset-0 bg-gradient-to-r from-amber-500 via-amber-300 to-amber-500"></div>
   </div>
       <div className='relative z-10'>
+      <span className="top-1 relative">
+            <Button
+            onClick={handleViewChange}
+            className='bg-transparent text-black hover:bg-transparent'
+          >
+            {currentView === 'month' ? <EyeIcon /> : currentView === 'week' ? <CalendarDaysIcon /> : <Calendar />}
+          </Button>  
+
+          </span>
         <div className="flex justify-between items-center mb-4">
-                <Button 
-                  onClick={() => setCurrentMonth(currentMonth.clone().subtract(1, 'month'))} 
-                  className='bg-amber-500 text-black hover:bg-amber-800'
-                > 
-                  Previous </Button>
+          <Button 
+            onClick={() => setCurrentMonth(currentMonth.clone().subtract(1, 'month'))} 
+            className='bg-amber-500 text-black hover:bg-amber-800'
+          > 
+            Previous </Button>
           <span className="rbc-toolbar-label text-amber-600 text-xl mx-4"> 
             {currentMonth.format('MMMM YYYY')}
           </span> 
-          
         <Button 
           onClick={() => setCurrentMonth(currentMonth.clone().add(1, 'month'))} 
           className='bg-amber-500 text-black hover:bg-amber-800'
         > 
-            Next </Button>
-            </div>
+          Next </Button>
+        </div>
           <BigCalendar
             onSelectEvent={handleSelectSlot}
+            selectable
             localizer={localizer}
             events={events}
             toolbar={false}
@@ -88,7 +107,7 @@ export const CalendarTask = ({ tasks }: CalendarTaskProps) => {
                 color: 'tan',
               }
             })}
-            defaultView='month'
+            view={currentView}
             components={{
               toolbar: ({ label }) => (
                 <div className="rbc-toolbar"> 
@@ -100,6 +119,7 @@ export const CalendarTask = ({ tasks }: CalendarTaskProps) => {
               ),
             }}
             date={currentMonth.toDate()} 
+            onView={(view) => setCurrentView(view)}
             onNavigate={(newDate) => setCurrentMonth(moment(newDate))}
             dayPropGetter={(date) => {
               const isCurrentMonth = date.getMonth() === currentMonth.month(); 
