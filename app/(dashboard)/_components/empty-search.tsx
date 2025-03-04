ef18de59410ad75cd88ff77c84ea4dce726e0e7d
GianniCrux <gianni.disti@gmail.com>
2025-03-04 17:34:08 +0100

    style(empty-search): enhance text styles and alignment for improved readability

--- app/(dashboard)/_components/empty-search.tsx
+++ app/(dashboard)/_components/empty-search.tsx
@@ -1,20 +1,15 @@
 import Image from "next/image";
 
 export const EmptySearch = () => {
-    return (
-        <div className="h-full flex flex-col items-center justify-center">
-            <Image 
-                src="/empty-search2.svg"
-                height={140}
-                width={140}
-                alt="Empty"
-            />
-            <h2 className="text-2xl font-semibold mt-6">
-                No results found!
-            </h2>
-            <p className="text-muted-foreground text-sm mt-2">
-                Try searching for something else.
-            </p>
-        </div>
-    );
+  return (
+    <div className="h-full flex flex-col items-center justify-center text-center">
+      <Image src="/empty-search2.svg" height={140} width={140} alt="Empty" />
+      <h2 className="text-2xl font-semibold mt-6 text-gray-800 dark:text-gray-100">
+        No results found!
+      </h2>
+      <p className="text-sm text-gray-600 dark:text-gray-400 mt-2">
+        Try searching for something else.
+      </p>
+    </div>
+  );
 };
