f4c196a7b5179261caa93f5df7c2358502194e82
GianniCrux <gianni.disti@gmail.com>
2025-03-04 17:34:31 +0100

    style(navbar): refine layout and styles for improved aesthetics and usability

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -1,9 +1,6 @@
 "use client";
 
-import { 
-    UserButton, 
-    useOrganization,
- } from "@clerk/nextjs";
+import { UserButton, useOrganization } from "@clerk/nextjs";
 import { SearchInput } from "./search-input";
 import { InviteButton } from "./invite-button";
 import Image from "next/image";
@@ -14,42 +11,39 @@ import DarkModeToggle from "./dark-mode-toggle";
 import AddressBook from "./address-book";
 
 const font = Poppins({
-    subsets: ["latin"],
-    weight: ["600"],
+  subsets: ["latin"],
+  weight: ["600"],
 });
 
 export const Navbar = () => {
-    const { organization } = useOrganization();
+  const { organization } = useOrganization();
 
-    return (
-        <div className="flex items-center gap-x-4 p-5 relative">
-            <Link href="/">
-                <div className="flex items-center gap-x-2">
-                    <Image 
-                        src="/plannerLogo.svg"
-                        alt="GPlanner"
-                        height={60}
-                        width={60}
-                    />
-                    <span className={cn(
-                        "font-semibold text-2xl hidden sm:block",
-                        font.className,
-                    )}>
-                        GPlanner
-                    </span>
-                </div>
-            </Link>
-            <div className="lg:flex-1">
-                <SearchInput />
-            </div>
-            {organization && (
-            <div className="hidden sm:block">
-            <InviteButton />
-            </div>
+  return (
+    <div className="flex items-center gap-x-4 p-5 relative bg-white dark:bg-gray-900 shadow-md">
+      <Link href="/">
+        <div className="flex items-center gap-x-2">
+          <Image src="/plannerLogo.svg" alt="GPlanner" height={60} width={60} />
+          <span
+            className={cn(
+              "font-semibold text-2xl hidden sm:block text-gray-800 dark:text-gray-100",
+              font.className
             )}
-            <AddressBook />
-            <DarkModeToggle />
-            <UserButton />
+          >
+            GPlanner
+          </span>
         </div>
-    );
+      </Link>
+      <div className="lg:flex-1">
+        <SearchInput />
+      </div>
+      {organization && (
+        <div className="hidden sm:block">
+          <InviteButton />
+        </div>
+      )}
+      <AddressBook />
+      <DarkModeToggle />
+      <UserButton />
+    </div>
+  );
 };
