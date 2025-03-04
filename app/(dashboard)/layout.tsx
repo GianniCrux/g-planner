e36819c91d7b9379ab41e1cbddb36f076a1072ab
GianniCrux <gianni.disti@gmail.com>
2025-03-04 17:32:05 +0100

    refactor(layout): simplify DashboardLayout component structure and improve styling

--- app/(dashboard)/layout.tsx
+++ app/(dashboard)/layout.tsx
@@ -2,30 +2,24 @@ import { ClientOrgSidebar } from "./_components/client-org-sidebar";
 import { Navbar } from "./_components/navbar";
 import { OrgSidebar } from "./_components/org-sidebar";
 
-
-
 interface DashboardLayoutProps {
-    children: React.ReactNode;
-};
+  children: React.ReactNode;
+}
 
-const DashboardLayout = ({
-    children,
-}: DashboardLayoutProps) => {
-    return (
-        <main className="h-full">
-            <div className="h-full">
-                <Navbar />
-                <div className="flex gap-x-3 h-full">
-                <div className="sm:pl-7">
-                    <ClientOrgSidebar />
-                </div>
-                <div className="h-full flex-1"> 
-                    {children}
-                </div>
-                </div>
-            </div>
-        </main>
-    );
+const DashboardLayout = ({ children }: DashboardLayoutProps) => {
+  return (
+    <main className="h-full bg-white dark:bg-gray-900">
+      <div className="h-full">
+        <Navbar />
+        <div className="flex gap-x-3 h-full">
+          <div className="">
+            <ClientOrgSidebar />
+          </div>
+          <div className="h-full flex-1">{children}</div>
+        </div>
+      </div>
+    </main>
+  );
 };
 
 export default DashboardLayout;
