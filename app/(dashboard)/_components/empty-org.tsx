160a2619937d246c596331a2ef45dfe787ce1075
GianniCrux <gianni.disti@gmail.com>
2025-03-04 17:33:56 +0100

    style(empty-org): enhance layout and text styles for improved readability and user experience

--- app/(dashboard)/_components/empty-org.tsx
+++ app/(dashboard)/_components/empty-org.tsx
@@ -1,40 +1,28 @@
-import Image from "next/image"
-import { CreateOrganization } from "@clerk/nextjs"
-import { Button } from "@/components/ui/button"
-
-import {
-    Dialog,
-    DialogContent,
-    DialogTrigger,
-} from "@/components/ui/dialog"
+import Image from "next/image";
+import { CreateOrganization } from "@clerk/nextjs";
+import { Button } from "@/components/ui/button";
+import { Dialog, DialogContent, DialogTrigger } from "@/components/ui/dialog";
 
 export const EmptyOrg = () => {
-    return (
-        <div className="h-full flex flex-col items-center justify-center">
-            <Image 
-                src="/org.svg"
-                alt="Empty"
-                height={200}
-                width={200}
-            />
-            <h2 className="text-2xl font-semibold mt-6">
-                Welcome to your planner
-            </h2>
-            <p className="text-muted-foreground text-sm mt-2">
-                Create an organization to get started
-            </p>
-            <div className="mt-6">  
-                <Dialog>
-                    <DialogTrigger asChild >
-                        <Button size="lg">
-                            Create organization
-                        </Button>
-                    </DialogTrigger>
-                    <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
-                        <CreateOrganization routing="hash"/>
-                    </DialogContent>
-                </Dialog>
-            </div>
-        </div>
-    )
-}
+  return (
+    <div className="h-full flex flex-col items-center justify-center text-center">
+      <Image src="/org.svg" alt="Empty" height={200} width={200} />
+      <h2 className="text-2xl font-semibold mt-6 text-gray-800 dark:text-gray-100">
+        Welcome to your planner
+      </h2>
+      <p className="text-sm text-gray-600 dark:text-gray-400 mt-2">
+        Create an organization to get started
+      </p>
+      <div className="mt-6">
+        <Dialog>
+          <DialogTrigger asChild>
+            <Button size="lg">Create organization</Button>
+          </DialogTrigger>
+          <DialogContent className="p-0 bg-transparent border-none max-w-[480px]">
+            <CreateOrganization routing="hash" />
+          </DialogContent>
+        </Dialog>
+      </div>
+    </div>
+  );
+};
