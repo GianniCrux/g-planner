65192662e3a61ba8b4bc847f6b54adaff7cb4176
GianniCrux <gianni.disti@gmail.com>
2025-03-04 17:34:25 +0100

    style(invite-button): improve button styles and dialog content for better user experience

--- app/(dashboard)/_components/invite-button.tsx
+++ app/(dashboard)/_components/invite-button.tsx
@@ -2,26 +2,31 @@ import { OrganizationProfile } from "@clerk/nextjs";
 import { Plus } from "lucide-react";
 
 import {
-    Dialog,
-    DialogContent,
-    DialogTrigger,
+  Dialog,
+  DialogContent,
+  DialogTrigger,
 } from "@/components/ui/dialog";
 import { Button } from "@/components/ui/button";
 
 export const InviteButton = () => {
-    return (
-        <Dialog>
-            <DialogTrigger asChild>
-                <Button variant="secondary" className="rounded-full bg-amber-500 hover:bg-amber-800">
-                    <Plus className="h-4 w-4 mr-2" />
-                    Invite members
-                </Button>
-            </DialogTrigger>
-            <DialogContent  className="bg-transparent border-none rounded-lg shadow-lg p-6 max-w-[950px] max-h-sm">
+  return (
+    <Dialog>
+      <DialogTrigger asChild>
+        <Button
+          variant="secondary"
+          className="rounded-full bg-amber-500 hover:bg-amber-600 text-white transition-colors"
+        >
+          <Plus className="h-4 w-4 mr-2" />
+          Invite members
+        </Button>
+      </DialogTrigger>
+      <DialogContent
+        className="bg-white dark:bg-gray-800 border border-gray-200 dark:border-gray-700 rounded-lg shadow-lg p-6 max-w-[950px] max-h-sm"
+      >
         <div className="max-h-[500px] max-w-[950px] overflow-auto scrollbar-hide">
-          <OrganizationProfile routing="hash"/>
-            </div>
-            </DialogContent>
-        </Dialog>
-    );
+          <OrganizationProfile routing="hash" />
+        </div>
+      </DialogContent>
+    </Dialog>
+  );
 };
