71a65c157eec8d0697275b9121888291330813d9
GianniCrux <gianni.disti@gmail.com>
2025-03-10 14:11:19 +0100

    refactor(middleware): replace authMiddleware with clerkMiddleware and update route matcher

--- middleware.ts
+++ middleware.ts
@@ -1,9 +1,12 @@
-import { authMiddleware } from "@clerk/nextjs/server";
+import { clerkMiddleware } from '@clerk/nextjs/server'
 
-export default authMiddleware({
-  publicRoutes: [],
-});
+export default clerkMiddleware()
 
 export const config = {
-  matcher: ["/((?!.+.[w]+$|_next).*)", "/", "/(api|trpc)(.*)"],
-};
+  matcher: [
+
+    '/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)',
+   
+    '/(api|trpc)(.*)',
+  ],
+}
