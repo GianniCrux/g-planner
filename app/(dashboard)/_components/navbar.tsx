1949425e760c7a85593ca4882c377e588360a17b
GianniCrux <gianni.disti@gmail.com>
2025-03-18 15:13:53 +0100

    feat(navbar): add 'Beta' label to GPlanner logo in navbar

--- app/(dashboard)/_components/navbar.tsx
+++ app/(dashboard)/_components/navbar.tsx
@@ -25,16 +25,21 @@ export const Navbar = () => {
   return (
     <div className="flex items-center gap-x-4 p-5 relative bg-white dark:bg-gray-900 shadow-md">
       <Link href="/">
-        <div className="flex items-center gap-x-2">
+        <div className="flex items-center gap-x-2 relative">
           <Image src="/plannerLogo.svg" alt="GPlanner" height={60} width={60} />
-          <span
-            className={cn(
-              "font-semibold text-2xl hidden sm:block text-gray-800 dark:text-gray-100",
-              font.className
-            )}
-          >
-            GPlanner
-          </span>
+          <div className="relative flex items-center">
+            <span
+              className={cn(
+                "font-semibold text-2xl hidden sm:block text-gray-800 dark:text-gray-100",
+                font.className
+              )}
+            >
+              GPlanner
+            </span>
+            <span className="absolute sm:-right-0 sm:top-2/3 sm:translate-y-1/2 right-0 top-0 translate-x-1/2 translate-y-1/2 text-[10px] bg-yellow-400 text-black px-1.5 py-0.5 rounded uppercase font-bold shadow">
+              Beta
+            </span>
+          </div>
         </div>
       </Link>
 
